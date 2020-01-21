20f7d526cf8081158960882058414708ae249f8d
Moses Ogwo <mosesogwo@gmail.com>
2020-01-21 11:04:05 +0100

    fix unique key for siblings in loop with crypto-js package

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,29 +1,27 @@
 import React from 'react';
+import sha256 from 'crypto-js/sha256';
 import Button from './Button';
 
 const ButtonPanel = () => {
   const buttonGroups = [
-    ["AC", "+/-", "%", "/"],
-    ["7", "8", "9", "X"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", ".", "="]
+    ['AC', '+/-', '%', '/'],
+    ['7', '8', '9', 'X'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '='],
   ];
 
-  const buttons = buttonGroups.map((buttonGroup, idx) => {
-  return (
-    <div className={"group-"+ idx }>
-      { buttonGroup.map((buttonName) => {
-        return (<Button name={buttonName}/>)
-        }) }
-    </div>)
-  })
+  const buttons = buttonGroups.map((buttonGroup, idx) => (
+    <div className={`group-${idx}`} key={sha256(buttonGroup.join())}>
+      { buttonGroup.map((buttonName) => (<Button name={buttonName} key={sha256(buttonName)} />)) }
+    </div>
+  ));
 
   return (
     <div className="buttonPanel">
       {buttons}
     </div>
-  )
+  );
 };
 
 export default ButtonPanel;
