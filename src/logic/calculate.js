c11b1b4aab3d480a0492840a45776b154262edff
Moses Ogwo <mosesogwo@gmail.com>
2020-01-22 13:05:36 +0100

    add skeletal structure for calculate method

---
+++ src/logic/calculate.js
@@ -0,0 +1,35 @@
+const calculate = (data, buttonName) => {
+  const { total, next, operation } = data;
+
+  if (buttonName === "+/-") {
+
+  }
+
+  if (buttonName === "+") {
+
+  }
+
+  if (buttonName === "-") {
+
+  }
+
+  if (buttonName === "×") {
+    
+  }
+
+  if (buttonName === "÷") {
+    
+  }
+
+  if (buttonName === "%") {
+    
+  }
+
+  if (buttonName === "AC") {
+
+  }
+
+  if (buttonName === "=") {
+    
+  }
+}
