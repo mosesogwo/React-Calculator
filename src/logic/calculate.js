533b5d95f800201292a5faee7bd998b5a38f1690
Moses Ogwo <mosesogwo@gmail.com>
2020-01-22 16:09:01 +0100

    fix eslint formatting errors

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,48 +2,48 @@ import operate from './operate';
 
 const calculate = (data, buttonName) => {
   const { total, next, operation } = data;
-  
+
   let newTotal = total;
   let newNext = next;
   let newOperation = operation;
 
   if (
-    buttonName === "+" ||
-    buttonName === "-" ||
-    buttonName === "×" ||
-    buttonName === "÷"
-    ){
-      newOperation = buttonName;
-      if(newNext) {
+    buttonName === '+'
+    || buttonName === '-'
+    || buttonName === '×'
+    || buttonName === '÷'
+  ) {
+    newOperation = buttonName;
+    if (newNext) {
       newTotal = operate(newTotal, newNext, newOperation);
       newNext = null;
       newOperation = null;
-    } 
-  } else if (buttonName === "%") {
-    if(newNext) {
-      newTotal = operate(newNext, "%");
+    }
+  } else if (buttonName === '%') {
+    if (newNext) {
+      newTotal = operate(newNext, '%');
       newNext = null;
       newOperation = null;
-    } else if(newTotal) {
-      newTotal = operate(newTotal, "%");
-      newNext = null;
-      newOperation = null;
-    }  
-  } else if (buttonName === "+/-") {
-    if(newNext) {
-      newTotal = operate(newNext, -1, "×");
     } else if (newTotal) {
-      newTotal = operate(newTotal, -1, "×")
+      newTotal = operate(newTotal, '%');
+      newNext = null;
+      newOperation = null;
+    }
+  } else if (buttonName === '+/-') {
+    if (newNext) {
+      newTotal = operate(newNext, -1, '×');
+    } else if (newTotal) {
+      newTotal = operate(newTotal, -1, '×');
     }
     newNext = null;
     newOperation = null;
-  } else if (buttonName === "AC") {
+  } else if (buttonName === 'AC') {
     newTotal = null;
     newNext = null;
     newOperation = null;
   }
 
-  return { newTotal, newNext, newOperation }
-}
+  return { newTotal, newNext, newOperation };
+};
 
 export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,19 +1,20 @@
 import Big from 'big.js';
 
-const operate = (numberOne, numberTwo=null, operation) => {
+const operate = (numberOne, numberTwo = null, operation) => {
   const bigNumOne = Big(numberOne);
   const bigNumTwo = Big(numberTwo);
-  if (operation === "+") {
+  if (operation === '+') {
     return (bigNumOne.plus(bigNumTwo));
-  } else if (operation === "-") {
-    return (bigNumOne.minus(bigNumTwo))
-  } else if (operation === "÷") {
-    return (bigNumOne.div(bigNumTwo))
-  } else if (operation === "×") {
-    return (bigNumOne.times(bigNumTwo))
-  } else if (operation === "%") {
-    return (bigNumOne.div(100))
-  } 
-}
+  } if (operation === '-') {
+    return (bigNumOne.minus(bigNumTwo));
+  } if (operation === '÷') {
+    return (bigNumOne.div(bigNumTwo));
+  } if (operation === '×') {
+    return (bigNumOne.times(bigNumTwo));
+  } if (operation === '%') {
+    return (bigNumOne.div(100));
+  }
+  return false;
+};
 
 export default operate;
