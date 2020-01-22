0a08a019bd0bf41680649ee92e38155f74c1da48
Moses Ogwo <mosesogwo@gmail.com>
2020-01-22 09:59:29 +0100

    sized buttons and positioned them using flex-basis

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = ({ name }) => (
-  <button type="button" id={ name }>{ name }</button>
+  <button type="button" id={ `btn-${name}` }>{ name }</button>
 );
 
 Button.propTypes = {
