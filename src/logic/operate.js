45952fe1834a90390080615a056bfd0249a65229
Moses Ogwo <mosesogwo@gmail.com>
2020-01-22 16:06:03 +0100

    setup calculate and operate functions, add calculate to App.js

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,16 +1,19 @@
-import 'big.js';
+import Big from 'big.js';
 
-const operate = (numberOne, numberTwo, operation) => {
+const operate = (numberOne, numberTwo=null, operation) => {
+  const bigNumOne = Big(numberOne);
+  const bigNumTwo = Big(numberTwo);
   if (operation === "+") {
-
+    return (bigNumOne.plus(bigNumTwo));
   } else if (operation === "-") {
-    
+    return (bigNumOne.minus(bigNumTwo))
   } else if (operation === "÷") {
-
+    return (bigNumOne.div(bigNumTwo))
   } else if (operation === "×") {
-
+    return (bigNumOne.times(bigNumTwo))
   } else if (operation === "%") {
-    
+    return (bigNumOne.div(100))
   } 
-
 }
+
+export default operate;
