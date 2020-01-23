246aa2431c29bb6a2d4ad1e5120ff1935d05b172
Moses Ogwo <mosesogwo@gmail.com>
2020-01-23 17:05:30 +0100

    add babel-esling as parser

--- src/components/App.js
+++ src/components/App.js
@@ -6,45 +6,44 @@ import calculate from '../logic/calculate';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       total: null,
       next: null,
       operation: null,
-   };
+    };
   }
 
   handleClick = (buttonName) => {
     const data = {
       total: this.state.total,
       next: this.state.next,
-      operation: this.state.operation
-    }
+      operation: this.state.operation,
+    };
 
     const newData = calculate(data, buttonName);
     this.setState({
       total: newData.total,
       next: newData.next,
-      operation: newData.operation
-    })
-    console.log(newData)
+      operation: newData.operation,
+    });
   }
 
   render() {
     const result = this.state.next ? this.state.next : this.state.total;
-    if (result){
-    return (
-      <div className="App">
-        <Display result = {result}/>
-        <ButtonPanel clickHandler = { this.handleClick }/>
-      </div>
-    );} else {
+    if (result) {
       return (
         <div className="App">
-          <Display />
-          <ButtonPanel clickHandler = { (buttonName) => this.handleClick(buttonName) }/>
+          <Display result={result} />
+          <ButtonPanel clickHandler={this.handleClick} />
         </div>
-      )
+      );
     }
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel clickHandler={(buttonName) => this.handleClick(buttonName)} />
+      </div>
+    );
   }
 }
 
